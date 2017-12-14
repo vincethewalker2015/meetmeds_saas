42c24f4079dc2ad4a0638536b9143fcd58d430c1
Vincent Walker <vincethewalker@yahoo.co.uk>
2017-12-14 16:59:29 +0000

    Initial Setup

--- Gemfile
+++ Gemfile
@@ -45,8 +45,12 @@ gem 'stripe', '1.16.1'
 # Use figaro to hide secret keys
 gem 'figaro', '1.0.0'
 
-# Use paperclip for image uploads
-gem 'paperclip', '4.2.1'
+# Use carrierwave for image uploads
+gem 'carrierwave', '1.1.0'
+gem 'mini_magick', '4.7.0'
+
+# Use Fog for S3 Image upload
+gem 'fog'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '3.1.7'
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,14 @@
 class Profile < ActiveRecord::Base
    belongs_to :user
+   mount_uploader :picture, PictureUploader
+   validate  :picture_size
    
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
-   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+   private
+    # Validates the size of the uploaded picture.
+    def picture_size
+       if picture.size > 5.megabytes
+           errors.add(:picture, "Hey! this picture should be less than 5MB")
+       end
+    end
+  
 end
