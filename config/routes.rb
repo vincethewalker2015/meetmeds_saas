59af9ffdfca82b24214b9e8b161e77d1c921b29d
Vincent Walker <vincethewalker@yahoo.co.uk>
2018-11-26 09:07:27 +0000

    Updated Security Issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resource :profile
   end
   resources :contacts
-  get '/about' => 'pages#about'
+  cd 
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
